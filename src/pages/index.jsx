ce2462847d215903342be1db95040723c20f2485
Krishna-hivoco <krishna@hivoco.com>
2025-08-23 14:01:56 +0530

    adding

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -429,7 +429,7 @@ export default function Home() {
                           {" "}
                           {mainStats?.share_count
                             ? mainStats?.share_count
-                            : 60930}
+                            : 82432}
                         </strong>
                         <span className="text-xs regular-text">
                           Shares & counting
@@ -515,7 +515,7 @@ export default function Home() {
                   >
                     <strong className=" text-3xl">
                       {" "}
-                      {mainStats?.share_count ? mainStats?.share_count : 60930}
+                      {mainStats?.share_count ? mainStats?.share_count : 82432}
                     </strong>
                     <span className="text-xs regular-text">
                       Shares & counting
